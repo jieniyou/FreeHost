b0342fd2223d626795293c4a531efbeee6170bd4
解你忧 <xiaomin.spring@qq.com>
2023-05-29 21:36:38 +0800

    解你忧 设置字符集-测试1

--- util/mysqlUtil.php
+++ util/mysqlUtil.php
@@ -30,6 +30,9 @@ function getMysqli(): mysqli {
     // 创建mysqli连接
     $conn = new mysqli($servername, $username, $password, $dbname);
 
+    // 设置字符集
+    $conn->set_charset('utf8mb4');
+
     // 检测连接
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
